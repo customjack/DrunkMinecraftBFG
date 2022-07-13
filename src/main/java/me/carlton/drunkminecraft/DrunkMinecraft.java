ba442fce552f5859fc8588250e116bf1cc17d191
custo <customjack@gmail.com>
2022-07-12 21:22:12 -0400

    commit test

---
+++ src/main/java/me/carlton/drunkminecraft/DrunkMinecraft.java
@@ -0,0 +1,17 @@
+package me.carlton.drunkminecraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DrunkMinecraft extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
